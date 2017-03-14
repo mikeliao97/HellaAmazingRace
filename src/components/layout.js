dd7008938bc178e6510fed5b68486a246fb1f019
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-13 22:38:32 -0700

    make src folder, create test home page

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+// src/components/Layout.js
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="app-content">Testing</div>
+        <footer>
+          <p>
+          Hella Amazing Race
+          </p>
+        </footer>
+      </div>
+    );
+  }
+}
