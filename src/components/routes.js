dd81e65fbd24a4a38b65fb85a1590d760e7f2118
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-13 22:48:56 -0700

    added landing page to routes

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/layout';
+import LandingPage from './components/LandingPage'
 
 const routes = (
   <Route path="/" component={Layout}>
+    <IndexRoute component={LandingPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
 );
