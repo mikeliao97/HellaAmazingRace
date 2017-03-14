de92561338629ed06d7aa6b96b4a1bca8acf8153
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-13 22:42:36 -0700

    added not found page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,3 @@
-// src/components/Layout.js
 import React from 'react';
 import { Link } from 'react-router';
 
---
+++ src/components/notFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+      </div>
+    );
+  }
+}
