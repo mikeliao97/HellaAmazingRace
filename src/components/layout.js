e9f5c297a54a1f47898a2b8702c18707d048d60b
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-14 16:08:17 -0700

    fixed rendering issue in Layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,11 +4,15 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Layout extends React.Component {
+  // removing this.props.children breaks routing for some reason, leave for now
   render() {
     return (
       <div>
+        <header>
+        </header>
+        <div>{this.props.children}</div>
         <footer>
-         hello worldasasdas
+         hello 
         </footer>
       </div>
     );
