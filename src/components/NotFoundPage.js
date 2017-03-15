26a5c36c16e53faea8d6c4998ac585be2e21060a
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-15 14:12:32 -0700

    fix not found link to redirect home

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -10,7 +10,7 @@ export default class NotFoundPage extends React.Component {
         <h1>404</h1>
         <h2>Page not found!</h2>
         <p>
-          <Link to="/">Go back to the main page</Link>
+          <Link to="/home">Go back to the main page</Link>
         </p>
       </div>
     );
