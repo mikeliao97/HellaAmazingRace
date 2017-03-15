2b628039dbbccc360fb4a2542bdeb945f419b4ac
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-15 15:35:34 -0700

    Added all buttons from mock-up sketch

--- src/components/CreateRace.js
+++ src/components/CreateRace.js
@@ -4,9 +4,23 @@ import React from 'react';
 
 export default class CreateRace extends React.Component {
   render() {
+
+    // const buttonSpace = {
+    //   height: '5px'
+    // };
+
     return (
       <div>
         <h1> Create a Race</h1>
+        <div className="text-center">
+          <button type="button" className="btn btn-primary">Set Start</button>
+          <button type="button" className="btn btn-primary">Set Checkpoint</button>
+          <button type="button" className="btn btn-primary">Set Finish</button>
+        </div>
+        <div className="text-center">
+          <button type="button" className="btn btn-primary">Save Race</button>
+          <button type="button" className="btn btn-primary">Start Race</button>
+        </div>
       </div>
     );
   }
