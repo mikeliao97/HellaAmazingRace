781e76074843a5284ff947023b5ab3d7c4794727
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-15 15:58:53 -0700

    added all buttons and added functions for future map use

--- src/components/CreateRace.js
+++ src/components/CreateRace.js
@@ -5,21 +5,40 @@ import React from 'react';
 export default class CreateRace extends React.Component {
   render() {
 
-    // const buttonSpace = {
-    //   height: '5px'
-    // };
+    let mapData = {
+      start: null,
+      finish: null,
+      checkpoints: []
+    };
+
+    const addMapData = (e) => {
+      let value = e.target.value;
+      if (value === 'start') {
+        mapData.start = 'test';
+      } else if (value === 'finish') {
+        mapData.finish = 'another test';
+      } else {
+        mapData.checkpoints.push('some data');
+      }
+      console.log(mapData);
+    };
+
+    const saveRace = () => {
+      console.log('save my race');
+    };
+
 
     return (
       <div>
-        <h1> Create a Race</h1>
+        <h1 className="text-center"> Create a Race</h1>
         <div className="text-center">
-          <button type="button" className="btn btn-primary">Set Start</button>
-          <button type="button" className="btn btn-primary">Set Checkpoint</button>
-          <button type="button" className="btn btn-primary">Set Finish</button>
+          <button type="button" className="btn btn-primary" value="start" onClick={addMapData}>Set Start</button>
+          <button type="button" className="btn btn-primary"onClick={addMapData}>Set Checkpoint</button>
+          <button type="button" className="btn btn-primary" value="finish" onclick={addMapData}>Set Finish</button>
         </div>
         <div className="text-center">
-          <button type="button" className="btn btn-primary">Save Race</button>
-          <button type="button" className="btn btn-primary">Start Race</button>
+          <button type="button" className="btn btn-primary" onClick={saveRace}>Save Race</button>
+          <button type="button" className="btn btn-primary" onClick={saveRace}>Start Race</button>
         </div>
       </div>
     );
