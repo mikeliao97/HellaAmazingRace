47e272dc3305693ee6c6b12e13f0e1e2e18568a7
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-15 12:56:50 -0700

    chenaged default page to login page

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import NotFoundPage from './components/NotFoundPage';
+import LoginPage from './components/LoginPage';
 
 const routes = (
   <Route path="/" component={Layout}>
-    <IndexRoute component={IndexPage}/>
+    <IndexRoute component={LoginPage}/>
+    <Route path="/home" component={IndexPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
 );
