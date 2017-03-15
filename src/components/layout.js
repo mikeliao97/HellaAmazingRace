769b814270238d5ad0bcacea23b606c842ad5cb9
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-15 15:22:29 -0700

    Added link back to homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,7 +23,7 @@ export default class Layout extends React.Component {
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul className="nav navbar-nav">
                         <li>
-                            <a href="#">About</a>
+                            <a href="home">Home</a>
                         </li>
                         <li>
                             <a href="CreateRace">Create Race</a>
