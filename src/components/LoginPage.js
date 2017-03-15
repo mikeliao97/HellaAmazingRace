70c3ee00bbc69542b810988ca42a30d956377d04
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-15 12:06:46 -0700

    work on meshing router with auth

---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+export default class LoginPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href="/auth/facebook">Log In with Facebook</a>
+      </div>
+    );
+  }
+}
