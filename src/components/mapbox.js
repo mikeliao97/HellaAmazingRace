b35ad530e9b3dde32d84c8696d4b3b3fc735daf0
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-20 12:51:18 -0700

    rendered googlemap for runrace

--- src/components/mapbox.js
+++ src/components/mapbox.js
@@ -7,45 +7,27 @@ export default class PubMap extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log(this);
   }
 
-  ComponentDidMount() {
-    this.fetchPosition();
+  componentDidMount() {
+    this.renderMap();
   }
 
-  fetchPosition() {
-    var lat = null;
-    var lng = null;
-
-    // sets your location as default
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var locationMarker = null;
-        if (locationMarker){
-          // return if there is a locationMarker bug
-          return;
-        }
-
-        lat = position.coords["latitude"];
-        lng = position.coords["longitude"];
-
-       console.log(lat, lng);
-
-      },
-      function(error) {
-        console.log("Error: ", error);
-      },
-      {
-        enableHighAccuracy: true
-      }
-      );
-    }
+  renderMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
   }
 
   render() {
     return (
-      <div> Hi </div>
+      <div id="map"></div>
     );
   }
 }
