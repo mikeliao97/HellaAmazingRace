172b3fea5a303fc85aa8121f1f7260d9876f2d10
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-20 14:07:58 -0700

    storing line coordinates for previous path, working towards adding pubnub tracking

--- src/components/mapbox.js
+++ src/components/mapbox.js
@@ -9,7 +9,8 @@ export default class PubMap extends React.Component {
     super(props);
     this.state = {
       lat: null,
-      lng: null
+      lng: null,
+      lineCoords: []
     }
   }
 
@@ -21,16 +22,18 @@ export default class PubMap extends React.Component {
           this.renderMap();
         }
       });
-    }, 5000);
+    }, 3000);
   }
 
   renderMap() {
-    var currLoc = {lat: this.state.lat, lng: this.state.lng};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    let currLoc = {lat: this.state.lat, lng: this.state.lng};
+    this.addLineCoordinates(currLoc);
+    // save map to window to be able to redraw as current location changes
+    window.map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: currLoc
     });
-    var marker = new google.maps.Marker({
+    window.marker = new google.maps.Marker({
       position: currLoc,
       map: map
     });
@@ -42,8 +45,44 @@ export default class PubMap extends React.Component {
         lat: location.coords.latitude,
         lng: location.coords.longitude
       });
-      cb(true);
+      cb('Done fetching location, ready.');
     })
+  }
+
+  addLineCoordinates(coords) {
+    let newCoords = this.state.lineCoords.slice();
+    newCoords.push(coords);
+    this.setState({
+      lineCoords: newCoords
+    });
+  }
+
+  redrawMap(payload) {
+    let lat = payload.message.lat;
+    let lng = payload.message.lng;
+
+    map.setCenter({lat:lat, lng:lng, alt:0});
+    marker.setPosition({lat:lat, lng:lng, alt:0});
+
+    this.addLineCoordinates(new google.maps.LatLng(lat, lng));
+
+    let lineCoordinatesPath = new google.maps.Polyline({
+      path: this.state.lineCoords,
+      geodesic: true,
+      strokeColor: '#2E10FF'
+    });
+
+    lineCoordinatesPath.setMap(map);
+  }
+
+  pubnubConnect() {
+    window.pnChannel = "map-channel";
+    var pubnub = new PubNub({
+      publishKey: 'pub-c-1e471fcb-f49a-481a-84ae-32b4e950ffa8',
+      subscribeKey: 'sub-c-00a667ae-0a73-11e7-9734-02ee2ddab7fe'
+    });
+    pubnub.subscribe({channels: [pnChannel]});
+    pubnub.addListener({message:this.redrawMap});
   }
 
   render() {
