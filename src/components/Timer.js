a86dc824b90707fda653acbc14be8c5ece86595e
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-22 12:25:18 -0700

    timer starts on first checkpoint, ends on last

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -11,60 +11,71 @@ const leftPad = (width, n) => {
 };
 
 export default class Stopwatch extends React.Component {
+
   constructor(props) {
     super(props);
     
-    ["lap", "update", "reset", "toggle"].forEach((method) => {
+    ['update', 'toggle'].forEach((method) => {
       this[method] = this[method].bind(this);
     });
 
     this.state = this.initialState = {
       isRunning: false,
-      lapTimes: [],
       timeElapsed: 0,
     };
   }
+
+  componentDidUpdate() {
+    if (this.props.complete) {
+      // stop max call stack
+      this.props.complete = false;
+
+      this.toggle();
+      const seconds = this.state.timeElapsed / 1000;
+      let formattedTime = {
+        min: Math.floor(seconds / 60).toString(),
+        sec: Math.floor(seconds % 60).toString(),
+        msec: (seconds % 1).toFixed(3).substring(2)
+      }
+      console.log(formattedTime, 'race over, stopping time');
+    }
+
+    if (this.props.running) {
+      // stop max call stack
+      this.props.running = false;
+      // starting race
+      this.toggle();
+    }
+  }
+
   toggle() {
     this.setState({isRunning: !this.state.isRunning}, () => {
       this.state.isRunning ? this.startTimer() : clearInterval(this.timer)
     });
   }
-  lap() {
-    const {lapTimes, timeElapsed} = this.state;
-    this.setState({lapTimes: lapTimes.concat(timeElapsed)});
-  }
-  reset() {
-    clearInterval(this.timer);
-    this.setState(this.initialState);
-  }
+
   startTimer() {
     this.startTime = Date.now();
     this.timer = setInterval(this.update, 10);
   }
+
   update() {
     const delta = Date.now() - this.startTime;
     this.setState({timeElapsed: this.state.timeElapsed + delta});
     this.startTime = Date.now();
   }
+
   render() {
-    const {isRunning, lapTimes, timeElapsed} = this.state;
+    const {isRunning, timeElapsed} = this.state;
     return (
       <div>
         <TimeElapsed id="timer" timeElapsed={timeElapsed} />
-        <button onClick={this.toggle}>
-          {isRunning ? 'Stop Race' : 'Start Race'}
-        </button>
-        <button
-          onClick={isRunning ? this.lap : this.reset}
-          disabled={!isRunning && !timeElapsed}
-         >
-          {isRunning || !timeElapsed ? 'Lap Race Time' : 'Reset Race Time'}
-        </button>
-        {lapTimes.length > 0 && <LapTimes lapTimes={lapTimes} />}
       </div>
     );
   }
 }
+
+
 
 class TimeElapsed extends React.Component {
   getUnits() {
@@ -75,6 +86,7 @@ class TimeElapsed extends React.Component {
       msec: (seconds % 1).toFixed(3).substring(2)
     };
   }
+
   render() {
     const units = this.getUnits();
     return (
@@ -83,26 +95,6 @@ class TimeElapsed extends React.Component {
         <span>{leftPad(2, units.sec)}.</span>
         <span>{units.msec}</span>
       </div>
-    );
-  }
-}
-
-class LapTimes extends React.Component {
-  render() {
-    const rows = this.props.lapTimes.map((lapTime, index) =>
-      <tr key={++index}>
-        <td>{index}</td>
-        <td><TimeElapsed timeElapsed={lapTime} /></td>
-      </tr>
-    );
-    return (
-      <table id="lap-times">
-        <thead>
-          <th>Lap</th>
-          <th>Time</th>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
     );
   }
 }
